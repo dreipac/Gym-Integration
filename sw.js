557c2bdb159ac8be060c0a9ba7caea596e1296b0
dreipac <armin.durmisi10@gmail.com>
2025-10-30 14:09:01 +0100

    Enhance service worker install and activate events
    Updated service worker to ensure immediate control of clients after activation.

--- sw.js
+++ sw.js
@@ -43,3 +43,16 @@ self.addEventListener("fetch", (e) => {
   );
 });
 
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+
+
